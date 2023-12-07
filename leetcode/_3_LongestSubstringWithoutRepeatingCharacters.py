3a9683792e4b24fc55b43dfba695412cbae723ee
Balkrishan Nagpal <balkrishan.nagpal@optum.com>
2023-12-07 21:13:18 +0530

    LC-3

--- leetcode/_3_LongestSubstringWithoutRepeatingCharacters.py
+++ leetcode/_3_LongestSubstringWithoutRepeatingCharacters.py
@@ -1,21 +1,23 @@
 class Solution:
 
+    if __name__ == "__main__":
+        print("Running _3_LongestSubstringWithoutRepeatingCharacters.py")
     def lengthOfLongestSubstring(self, s: str) -> int:
-        indexes = {}
+        symbols = [-1]*255
 
-        length, start = 0, 0
+        start, max_len = 0, 0
 
-        for i, ch in enumerate(s):
-            index = indexes.get(ch)
-            if index is not None and index >= start:
-                length = max(length, i - start)
-                start = index + 1
-            indexes[ch] = i
+        for i in range(len(s)) :
+            char = s[i]
+            char_to_int = ord(char)
+            curr_index = symbols[char_to_int]
+            if curr_index != -1:
+                if curr_index >= start:
+                    max_len = max(max_len, i-start)
+                    start = curr_index +1
+            symbols[char_to_int] = i
 
-        length = max(length, len(s) - start)
-        return length
-
+        return max(max_len, len(s)- start)
 
 sln = Solution()
-s = "abcabcbb"
-print(sln.lengthOfLongestSubstring(s))
+print("Max length : ", sln.lengthOfLongestSubstring("abcabcbb"))
