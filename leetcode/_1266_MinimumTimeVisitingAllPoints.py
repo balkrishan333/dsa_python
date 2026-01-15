a2124aa2296817a42c590e68b87fd6e4b2df2457
Nagpal, Balkrishan <balkrishan.nagpal@optum.com>
2026-01-15 18:44:09 +0530

    LC-1266

---
+++ leetcode/_1266_MinimumTimeVisitingAllPoints.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        ans = 0
+        for i, point in enumerate(points):
+            currX = point[0]
+            currY = point[1]
+
+            if i < len(points)-1:
+                next_point = points[i+1]
+                targetX = next_point[0]
+                targetY = next_point[1]
+
+                #using chess distance formula
+                ans += max(abs(currX-targetX), abs(currY-targetY))
+
+        return ans
+
+sln = Solution()
+points = [[1,1],[3,4],[-1,0]]
+print(sln.minTimeToVisitAllPoints(points))
+
+
+
